bc870d701cbca4bc56d1391e91b12298fb64043e
daviddgonzalez <daviddgonzalez56@gmail.com>
2025-11-08 01:59:52 -0500

    testing llq changes

--- LLQ.hpp
+++ LLQ.hpp
@@ -14,13 +14,25 @@ public:
     LLQ()=default;
 
     // Insertion
-    void enqueue(const T& item) override{addTail(item);}
+    void enqueue(const T& item) override{list.addTail(item);}
 
     // Deletion
-    T dequeue() override{return(list.removeHead());}
+    T dequeue() override{
+        if (list.getCount()==0) {
+            throw(std::runtime_error("Tried to pop w no eles"));
+        }
+        T temp = list.getHead()->data;
+        list.removeHead();
+        return(temp);
+    }
 
     // Access
-    T peek() const override{return(list.getHead());}
+    T peek() const override {
+        if (list.getCount()==0){
+            throw(std::runtime_error("Tried to access w no eles"));
+        }
+        return(list.getHead()->data);
+    }
 
     // Getter
     std::size_t getSize() const noexcept override{return(list.getCount());}
