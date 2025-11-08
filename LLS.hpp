83199f9c1ada12961a031f11a070dd5de37f1396
daviddgonzalez <daviddgonzalez56@gmail.com>
2025-11-08 02:02:11 -0500

    made stack changes

--- LLS.hpp
+++ LLS.hpp
@@ -16,10 +16,23 @@ public:
     void push(const T& item) override {list.addHead(item);}
 
     // Deletion
-    T pop() override {return(list.removeHead());}
+    T pop() override {
+        if (list.getCount()==0) {
+        throw(std::runtime_error("Tried to pop w no eles"));
+        }
+        T temp = list.getHead()->data;
+        list.removeHead();
+        return(temp);
+    }
 
     // Access
-    T peek() const override{return(list.getHead());}
+    T peek() const override {
+        if (list.getCount()==0){
+            throw(std::runtime_error("Tried to access w no eles"));
+        }
+        return(list.getHead()->data);
+
+    }
 
     //Getters
     std::size_t getSize() const noexcept override{return(list.getCount());}
